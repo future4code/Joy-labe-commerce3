fba303c153203bac746b7ffbe95f5719df88ed9a
Natalia Alves <nataliavalins@gmail.com>
2021-11-29 22:15:28 -0300

    nomes-de-produtos-e-valores-com-props

--- src/components/CardProdutos.jsx
+++ src/components/CardProdutos.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import styled from "styled-components"  
 
-class CardProdutos extends React.Component {
-    render() {
-        return (
-            <div>
-                <img src="https://picsum.photos/id/237/200/300" /> 
+const TituloProduto = styled.h4`
+    color: blue;
+`
+const PreçoProduto = styled.h4`
+    color: red;
+`
+
+const CardProdutos = (props) => {
+    
+    return (
+        <div>
+            <img src="https://picsum.photos/id/237/200/300" /> 
                 <div>
-                    <p> Produto X </p>
-                    <p> R$200,00 </p>
+                    <p><TituloProduto>{props.tituloProduto}</TituloProduto></p>
+                    <p><PreçoProduto>{props.preçoProduto}</PreçoProduto></p>
                     <button> Adicionar ao carrinho </button>
                 </div>
-            </div>)
-        }
+        </div>)
+    
 }
 
 export default CardProdutos;
